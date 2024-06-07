6afa7292525e4a5a3c4d7e49e1eeee02ac868140
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-08 02:51:08 +0630

    implement: Schema For User Database

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("User", userSchema);
