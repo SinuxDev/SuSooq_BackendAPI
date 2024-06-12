aae76f6cad3e8c9e3b580bdc72d6737187cc4a12
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-13 00:44:53 +0630

    create: Schema For Product

---
+++ models/Product.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    usedFor: {
+      type: String,
+      required: true,
+    },
+    status_details: {
+      type: Array,
+      default: null,
+    },
+    status: {
+      type: Boolean,
+      default: "pending",
+    },
+    seller: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Products", productSchema);
