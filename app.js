664238d0c98dc6b9a5931c969437d672102f3f19
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-14 23:34:53 +0630

    config: Multer For Image Uploads

--- app.js
+++ app.js
@@ -2,18 +2,44 @@ const express = require("express");
 const mongooes = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const multer = require("multer");
 require("dotenv").config();
 
 const app = express();
 
-//Routes
+//Routes Path
 const authRoutes = require("./routes/auth");
 const productRoutes = require("./routes/product");
 
+// Multer Config
+const storageConfig = multer.diskStorage({
+  filename: (req, file, cb) => {
+    const surfix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, surfix + "-" + file.originalname);
+  },
+});
+
+const filterConfig = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, undefined);
+  }
+};
+
 // Global Middleware
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors({ origin: "*" }));
+app.use(
+  multer({ storage: storageConfig, fileFilter: filterConfig }).array(
+    "product_images"
+  )
+);
 
 // Routes
 app.use(authRoutes);
