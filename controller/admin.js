5cf02db76396043764e179a38de3f297bab044e6
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-18 01:44:25 +0630

    fun: [getAllProducts]

--- controller/admin.js
+++ controller/admin.js
@@ -1,1 +1,26 @@
-exports.getAllProducts = async (req, res) => {};
+const Product = require("../models/Product");
+
+exports.getAllProducts = async (req, res) => {
+  try {
+    const productDoc = await Product.find()
+      .populate("seller", "name")
+      .sort({ createdAt: -1 });
+
+    if (!productDoc) {
+      return res.status(404).json({
+        isSuccess: false,
+        message: "Products not found",
+      });
+    }
+
+    return res.status(200).json({
+      isSuccess: true,
+      products: productDoc,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      isSuccess: false,
+      message: "Internal Server Error",
+    });
+  }
+};
