9d3182ab803fdb8eab5399f07079ca1621fe4473
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-18 01:30:21 +0630

    modified: Admin Middleware [isAdmin]

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,13 +1,33 @@
+const User = require("../models/user");
+
 const isAdmin = async (req, res, next) => {
   const UNAUTHORIZED_MESSAGE = "Unauthorized Admin Access";
+
   try {
     const { userId } = req;
-    console.log(userId);
+
+    if (!userId) {
+      return res.status(401).json({
+        isSuccess: false,
+        message: UNAUTHORIZED_MESSAGE,
+      });
+    }
+
+    const userDoc = await User.findById(userId).select("role");
+
+    if (!userDoc || userDoc.role !== "admin") {
+      return res.status(401).json({
+        isSuccess: false,
+        message: UNAUTHORIZED_MESSAGE,
+      });
+    }
+
+    req.userId = userId;
     next();
   } catch (err) {
-    return res.status(401).json({
+    return res.status(500).json({
       isSuccess: false,
-      message: UNAUTHORIZED_MESSAGE,
+      message: "Internal Server Error",
     });
   }
 };
