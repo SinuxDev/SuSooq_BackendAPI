3d0d226822c29f499f1374faff4f4fbc859c029c
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-18 23:15:36 +0630

    controller: Product [ Approve & Reject & Rollback]

--- controller/admin.js
+++ controller/admin.js
@@ -24,3 +24,53 @@ exports.getAllProducts = async (req, res) => {
     });
   }
 };
+
+const updateProductStatus = async (req, res, status) => {
+  const { id } = req.params;
+  try {
+    const productDoc = await Product.findById(id);
+
+    if (!productDoc) {
+      return res.status(404).json({
+        isSuccess: false,
+        message: "Product not found",
+      });
+    }
+
+    productDoc.status = status;
+    await productDoc.save();
+
+    let message;
+    switch (status) {
+      case "approved":
+        message = "Product approved successfully";
+        break;
+      case "rejected":
+        message = "Product rejected successfully";
+        break;
+      case "pending":
+        message = "Product status rolled back successfully";
+        break;
+      default:
+        message = "Product status updated successfully";
+    }
+
+    return res.status(200).json({
+      isSuccess: true,
+      message,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      isSuccess: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+exports.ApproveProduct = (req, res) =>
+  updateProductStatus(req, res, "approved");
+
+exports.RejectProduct = (req, res) => updateProductStatus(req, res, "rejected");
+
+exports.RollbackProduct = (req, res) =>
+  updateProductStatus(req, res, "pending");
