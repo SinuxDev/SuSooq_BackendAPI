b79b1c55bcd370c0b6729d3dd38c86e3de24bc85
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-26 00:34:32 +0630

    fun : FilterProducts

--- controller/public.js
+++ controller/public.js
@@ -25,3 +25,39 @@ exports.getAllProducts = async (req, res, next) => {
     });
   }
 };
+
+exports.filterProducts = async (req, res, next) => {
+  try {
+    const { searchQuery } = req.query;
+
+    if (!searchQuery || typeof searchQuery !== "string") {
+      return res.status(400).json({
+        isSuccess: false,
+        message: "Invalid Search Query",
+      });
+    }
+
+    const products = await Product.find({
+      status: "approved",
+      $and: [{ name: { $regex: searchQuery, $options: "i" } }],
+    }).sort({ createdAt: -1 });
+
+    if (!products || products.length <= 0) {
+      return res.status(404).json({
+        isSuccess: false,
+        message: "Products not found",
+      });
+    }
+
+    return res.status(200).json({
+      isSuccess: true,
+      message: "Products fetched successfully",
+      products,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      isSuccess: false,
+      message: err.message,
+    });
+  }
+};
