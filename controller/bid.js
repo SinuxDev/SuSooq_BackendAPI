1cc4ebc2134ef9b26976430bb24f5fdc153d0b97
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-28 23:49:48 +0630

    create: getBids

--- controller/bid.js
+++ controller/bid.js
@@ -30,3 +30,32 @@ exports.saveNewBid = async (req, res) => {
     });
   }
 };
+
+// Get all bids by product ID
+exports.getBidByProductId = async (req, res) => {
+  const { product_id } = req.params;
+  try {
+    const bids = await Bid.find({ product_id })
+      .populate("buyer_id", "name email")
+      .select("comment phone_num createdAt")
+      .sort({ createdAt: -1 });
+
+    if (!bids || bids.length === 0) {
+      return res.status(400).json({
+        isSuccess: false,
+        message: "Failed to fetch bids",
+      });
+    }
+
+    return res.status(200).json({
+      isSuccess: true,
+      message: "Bids fetched successfully",
+      bids,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      isSuccess: false,
+      message: "Internal server error",
+    });
+  }
+};
