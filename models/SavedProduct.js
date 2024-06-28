b67f247e64f3d17e1bc544274f30daf7bde8387a
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-29 02:19:01 +0630

    fixed: Issue#1

--- models/SavedProduct.js
+++ models/SavedProduct.js
@@ -21,4 +21,8 @@ const savedProductSchema = new Schema(
 // Create a unique index on the combination of user_id and product_id
 savedProductSchema.index({ user_id: 1, product_id: 1 }, { unique: true });
 
+// Issue#1 fixed : Added unique index on user_id and product_id combination to prevent duplicate entries
+// MongoDB Client Index Creation Command: db.savedproducts.createIndex({user_id: 1, product_id: 1}, {unique: true})
+// MongoDb Client : db.savedproducts.aggregate([{$group: {_id: {user_id: "$user_id", product_id: "$product_id"}, count: {$sum: 1}}}, {$match: {count: {$gt: 1}}}])
+
 module.exports = model("SavedProducts", savedProductSchema);
