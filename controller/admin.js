bb4eabc7fac6eb3add06ba65f93242fbdabe3dfb
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-30 01:59:31 +0630

    update:  Fun [getAllProducts]

--- controller/admin.js
+++ controller/admin.js
@@ -2,21 +2,56 @@ const Product = require("../models/Product");
 const User = require("../models/user");
 
 exports.getAllProducts = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const perPage = parseInt(req.query.perPage) || 10;
+
   try {
     const productDoc = await Product.find()
       .populate("seller", "name")
-      .sort({ createdAt: -1 });
+      .sort({ createdAt: -1 })
+      .skip((page - 1) * perPage)
+      .limit(perPage);
 
-    if (!productDoc) {
+    if (!productDoc.length) {
       return res.status(404).json({
         isSuccess: false,
         message: "Products not found",
       });
     }
 
+    const totalProducts = await Product.countDocuments();
+
+    const pendingProducts = await Product.countDocuments({ status: "pending" });
+
+    const totalPriceAggregation = await Product.aggregate([
+      {
+        $addFields: {
+          priceNumeric: { $toDouble: "$price" },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalPrice: { $sum: "$priceNumeric" },
+        },
+      },
+    ]);
+
+    const totalPrice =
+      totalPriceAggregation.length > 0
+        ? totalPriceAggregation[0].totalPrice
+        : 0;
+
+    const totalPages = Math.ceil(totalProducts / perPage);
+
     return res.status(200).json({
       isSuccess: true,
       products: productDoc,
+      totalPages,
+      totalProducts,
+      currentPage: page,
+      totalPrice,
+      pendingProducts,
     });
   } catch (err) {
     return res.status(500).json({
